ee3f90f18a3c7cfa7ad2d68f34c720509d9e2aec
SzabolcsMarton <maszabolcs@gmail.com>
2021-11-09 16:56:01 +0100

    project nr. 03 finished tested in main.

---
+++ src/main/java/numbers/PerfectNumbers.java
@@ -0,0 +1,17 @@
+package numbers;
+
+public class PerfectNumbers {
+
+    public boolean isPerfectNumber(int number) {
+        if (number == 1) {
+            return false;
+        }
+        int sum = 1;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == number;
+    }
+}
